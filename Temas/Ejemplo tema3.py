decf9b60abdc0def0a2d372663de9ceb8149ba31
neon520 <javierpg520@gmail.com>
2015-10-03 13:28:57 +0200

    Programa en Python usado en la explicación de un ejemplo en el tema 3

---
+++ Temas/Ejemplo tema3.py
@@ -0,0 +1,5 @@
+def mensaje (edad, mensaje = "Mi padre tiene") :
+	print(mensaje, edad)
+
+mensaje(35)						# Esto devuelve "Mi padre tiene 35"
+mensaje(34, "Yo tengo")			# Esto devuelve "Yo tengo 34"
