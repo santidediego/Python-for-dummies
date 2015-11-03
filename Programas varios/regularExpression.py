ae87c73b86a6d32193bf5ec13cb0a0c8357ba1ce
neon520 <javierpg520@gmail.com>
2015-11-03 12:09:01 +0100

    Corrigiendo un error analizando tarjetas de crédito, admitía 1234-1234 1234-1234 y no es válida

--- Programas varios/regularExpression.py
+++ Programas varios/regularExpression.py
@@ -9,6 +9,6 @@ def main():
         print (str+" es una palabra seguida de una letra mayúscula")
     if re.match('\w+@(\w+)\.com|es',str):
         print (str+" es un correo electrónico")
-    if re.match('(\d{4}(-| )){3}\d{4}',str):
+    if re.match('(((\d{4}-){3})|((\d{4} ){3}))\d{4}',str):
         print (str+" es una tarjeta de crédito")
 main()
