0486277a420b5b1ad23ba37b3826c19168dab50f
Santiago de Diego <santidediego@hotmail.com>
2015-11-17 11:58:18 +0100

    Añadido historial mongo

--- Flask/inicio.py
+++ Flask/inicio.py
@@ -10,6 +10,10 @@ import dbm
 from pymongo import MongoClient
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+__count__=0
+__first_time__=True
+
 #Las dos siguientes sentencias sirven para que funcione shelve. Solo necesario si trabajamos con la extension
 #app.config['SHELVE_FILENAME'] = 'shelve.db'
 #shelve.init_app(app)
@@ -19,6 +23,37 @@ client = MongoClient('mongodb://localhost:27017/')
 database = client['Mongo_DB']
 data_collection = database.datos
 
+"""
+Historial
+-"""
+
+def deleteFirst(lista):
+    for j in range(2):
+        lista[str(j)]=lista[str(j+1)]
+def save_hist(request):
+    global __count__
+    global __first_time__
+    if __count__ == 2:
+        if __first_time__:
+            __first_time__=False
+        else:
+            deleteFirst(session)
+        session[str(__count__)]=str(request.url)
+    else:
+        session[str(__count__)]=str(request.url)
+        __count__+=1
+
+def html_sessions():
+    global __count__
+    ses_html=list()
+    for j in range(0,__count__):
+        ses_html.append(session[str(j)])
+    return ses_html
+
+def invalidPassword(form,field):
+    if form.username.data in db:
+        if not db[form.username.data] == bytes(field.data,'utf-8') :
+            raise validators.ValidationError('Contraseña incorrecta')
 
 def guardar_datos(form):
         datos_usuario= {
@@ -35,7 +70,8 @@ def guardar_datos(form):
 class Login(Form):
     username = TextField('Nombre de Usuario', [validators.Length(min=4, max=25)])
     password = PasswordField('Contraseña', [
-        validators.Required()
+        validators.Required(),
+        invalidPassword
     ])
 
 class Formulario2(Form):
@@ -62,10 +98,14 @@ class Formulario2(Form):
 
 @app.route("/", methods=['GET', 'POST'])
 def inicio():
-    return render_template("inicio.html")
+     global __count__
+     save_hist(request)
+     return render_template("inicio.html", sesiones=html_sessions())
 
 @app.route('/formulario', methods=['GET', 'POST'])
 def register():
+    global __count__
+    save_hist(request)
     form = Formulario2(request.form)
     db_datos={}
     if request.method == 'POST' and form.validate():
@@ -74,33 +114,37 @@ def register():
         guardar_datos(form)
         session['username'] = form.username.data  #Lo almacenamos en las sesiones
         return redirect('/')
-    return render_template("formulario.html", form=form)
+    return render_template("formulario.html", form=form, sesiones=html_sessions())
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global __count__
+    save_hist(request)
     form=Login(request.form)
     user=form.username.data
     if 'username' in session: #Si hay una sesion activa
         Logeado=True
-        return render_template("login.html",form=form,Logeado=Logeado)
-    elif request.method == 'POST' and user in db: #Si está registrado
+        return render_template("login.html",form=form,Logeado=Logeado, sesiones=html_sessions())
+    elif request.method == 'POST' and form.validate() and user in db: #Si está registrado
         session['username'] = form.username.data #Lo almacenamos en las sesiones
-        db_datos={} #Reinicializamos datos
         return redirect('/')
     elif request.method == 'POST' and user not in db:
         return redirect('/formulario') #Redireccionamos al formulario de registro
     else:
         Logeado=False
-        return render_template("login.html",form=form,Logeado=Logeado)
+        return render_template("login.html",form=form,Logeado=Logeado, sesiones=html_sessions())
 
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
+    save_hist(request)
     session.pop('username', None)
     return redirect('/')
 
 @app.route('/visualizar')
 def visualizar():
+    global __count__
+    save_hist(request)
     if 'username' in session:
         user=session['username']
         '''
@@ -109,7 +153,7 @@ def visualizar():
         dic={}
         dic=data_collection.find_one({"Usuario: ": user})
 
-        return render_template("visualizar.html",dic=dic)
+        return render_template("visualizar.html",dic=dic, sesiones=html_sessions())
     else:
         return redirect('/')
 
